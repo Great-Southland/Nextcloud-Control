5482858ec2ec7c98419cc2240b01002e47d92b4a
Benjamin Forg <b.l.forgd.c.forg@gmail.com>
2020-03-15 20:46:11 +1030

    Modified functions to work properly

--- includes/core-functions.php
+++ includes/core-functions.php
@@ -102,7 +102,7 @@ function xml_to_array($raw_xml_data) {
 function get_create_nextcloud_share_link($method, $file_path, $shareType, $publicUpload, $permissions, $shareWith) {
 	//******************* Send API Request *************************
 	$url = 'ocs/v2.php/apps/files_sharing/api/v1/shares?shareWith='. $shareWith .'&shareType='. $shareType .'&publicUpload='. $publicUpload .'&permissions='. $permissions .'&path='. $file_path;
-	$xml_response = nc_request($method, $url)
+	$xml_response = nc_request($method, $url);
   $xml_in_array = xml_to_array($xml_response['body']);
 // ************** check if share link is in the "element" or "data" array (If it created the link its in the "data" array)******************
 	if (isset($xml_in_array['data']['element']['url'])) {
@@ -116,14 +116,14 @@ function get_create_nextcloud_share_link($method, $file_path, $shareType, $publi
 return $result;
 }
 
-/* ---------------------------- Get share link and if not created yet create and return the link ----------------------------*/
+/* =========================== Get share link and if not created yet create and return the link ======================*/
 function get_nc_share_link($file_path, $shareType = 3, $publicUpload = false, $permissions = 1, $shareWith = '') {
-	$get_create_nc_share_link = get_create_nextcloud_share_link('GET', $file_path, $shareType, $publicUpload, $permissions, $shareWith);
+	$get_create_nc_share_link = get_create_nextcloud_share_link_1('GET', $file_path, $shareType, $publicUpload, $permissions, $shareWith);
 // **************** Check if GET link returns false, if it does Create link ***************
 	if ($get_create_nc_share_link != false) {
 		$result = $get_create_nc_share_link;
 	}	else {
-		$result = get_create_nextcloud_share_link('POST', $file_path, $shareType, $publicUpload, $permissions, $shareWith);
+		$result = get_create_nextcloud_share_link_1('POST', $file_path, $shareType, $publicUpload, $permissions, $shareWith);
 	}
 // if it couldn't Get or create link return message
   if (empty($result)) {
@@ -131,3 +131,28 @@ function get_nc_share_link($file_path, $shareType = 3, $publicUpload = false, $p
 	}
 	return $result;
 }
+
+function display_nc_share_links() {
+	$url = 'https://Benjamin:benjaminforg@salones-portal.ddns.net/cloud/remote.php/dav/files/Benjamin/testgroupfolder';
+	$args = array( 'method' => 'PROPFIND',
+								 'timeout' => 40000,
+							 );
+
+
+	$raw_xml_response = wp_remote_request( $url, $args );
+	$xml_response = str_replace('d:', '', $raw_xml_response['body']);
+	$xml_array = xml_to_array($xml_response);
+	$result = '';
+
+	foreach($xml_array['response'] as $response){
+		$folder_path = 'testgroupfolder/';
+		$url_path = str_replace('/cloud/remote.php/dav/files/Benjamin/', '', $response['href']);
+		$file_name = str_replace($folder_path, '', $url_path);
+		$decoded_file_name = urldecode($file_name);
+
+		$share_url = get_nc_share_link_1($url_path);
+		$result .= '<a href="'. $share_url .'">'. $decoded_file_name .'</a><br>';
+	}
+
+	return $result;
+} add_shortcode('display_nc_share_link', 'display_nc_share_links');
