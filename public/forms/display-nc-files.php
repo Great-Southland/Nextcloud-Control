bd4163078e5501d1e66b7fb497effb49f844c36c
Benjamin Forg <b.l.forgd.c.forg@gmail.com>
2020-03-16 12:34:07 +1030

    Tidied up code

--- public/forms/display-nc-files.php
+++ public/forms/display-nc-files.php
@@ -6,7 +6,6 @@ function display_nc_share_links($shortcode_atts) {
 																				),$shortcode_atts);
 	$folder_path = $shortcode_att['folder-path'];
 	$hide_file = $shortcode_att['hide-file'];
-
 	$url = 'https://Benjamin:benjaminforg@salones-portal.ddns.net/cloud/remote.php/dav/files/Benjamin/'. $folder_path;
 	$args = array( 'method' => 'PROPFIND',
 								 'timeout' => 40000,
@@ -16,8 +15,6 @@ function display_nc_share_links($shortcode_atts) {
 	$xml_response = str_replace('d:', '', $raw_xml_response['body']);
 	$xml_array = xml_to_array($xml_response);
 	$result = '';
-
-
 
 	foreach($xml_array['response'] as $response){
 		$url_path = str_replace('/cloud/remote.php/dav/files/Benjamin/', '', $response['href']);
@@ -29,8 +26,5 @@ function display_nc_share_links($shortcode_atts) {
 		$share_url = get_nc_share_link($url_path);
 		$result .= '<a href="'. $share_url .'">'. $decoded_file_name .'</a><br>';
 	}
-
-
-
 	return $result;
 } add_shortcode('display-nc-share-link', 'display_nc_share_links');
