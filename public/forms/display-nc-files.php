9ff9cc5cad4ae5fe9abb4a514ad5c5018cf36d35
David-Forg <davidforgaus@gmail.com>
2020-03-18 21:15:12 +1030

    Reset display files transiant if files in folder have changed

--- public/forms/display-nc-files.php
+++ public/forms/display-nc-files.php
@@ -60,10 +60,28 @@ if(false === ($display_nextcloud_files = get_transient('display_nextcloud_files'
 		//Set Transient
 		set_transient('display_nextcloud_files', $result, DAY_IN_SECONDS);
 	} else {
+
+// Get all the variables for the xml_array for PROPFIND
+		$options = get_option( 'wnus_options', wnus_options_default() );
+		$nc_file_username = isset( $options['file_display_username']) ? sanitize_text_field( $options['file_display_username'] ) : '';
+		$nc_file_pass = isset( $options['file_display_pass']) ? sanitize_text_field( $options['file_display_pass'] ) : '';
+		$shortcode_att = shortcode_atts(array('folder-path'=>'',
+																					'hide-files'=>'Readme.md',),$shortcode_atts);
+		$folder_path = $shortcode_att['folder-path'];
+		$url = 'remote.php/dav/files/'. $nc_file_username .'/'. $folder_path;
+		$raw_xml_response = nc_request('PROPFIND', $url, $nc_file_username, $nc_file_pass);
+		$xml_response = str_replace('d:', '', $raw_xml_response['body']);
+		$xml_array = xml_to_array($xml_response);
+
+//if xml_array is not equal to the transiant of the PROPFIND list set PROPFIND to xml_array, delete the file display transiant and restart function from beginning
+		if ($xml_array != get_transient('display_nextcloud_files_list')) {
+
+			set_transient('display_nextcloud_files_list', $xml_array, DAY_IN_SECONDS);
+			delete_transient('display_nextcloud_files');
+			return display_nc_share_links($shortcode_atts);
+		}
+
 		$result =  get_transient('display_nextcloud_files');
 	}
 	return $result;
-}
-
-
-add_shortcode('list-nc-files', 'display_nc_share_links');
+} add_shortcode('list-nc-files', 'display_nc_share_links');
